a321b7ac304f55189e7ec09cab5fa80ec559aed7
Davide -limaCAT- Inglima <limacat@live.it>
2018-08-25 00:16:53 +0200

    Test project for CMake

---
+++ 01CMake/01CMake/01CMake.cpp
@@ -0,0 +1,21 @@
+﻿// 01CMake.cpp : Defines the entry point for the application.
+//
+
+#include "01CMake.h"
+
+int main()
+{
+
+#ifdef HELLO
+	std::cout << "Hello " << HELLO << std::endl;
+#elif
+	std::cout << "Hello CMake." << std::endl;
+#endif
+
+#ifdef CONFIG
+	std::cout << "Running " << CONFIG << std::endl;
+#endif
+
+	std::cin.ignore();
+	return 0;
+}
---
+++ del1/del1/del1.cpp
@@ -0,0 +1,12 @@
+﻿// del1.cpp : Defines the entry point for the application.
+//
+
+#include "del1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
